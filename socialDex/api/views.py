182dab326f4d1be036636096089468b8d72e6b27
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-07-16 13:47:53 +0200

    Add report to Redis

--- socialDex/api/views.py
+++ socialDex/api/views.py
@@ -1,5 +1,7 @@
 from api.models import Post
 from api.serializers import PostModelSerializer
+from datetime import datetime
+from redis import Redis
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -18,6 +20,11 @@ def posts(request):
 
     post_list = Post.objects.all().order_by('-id')
     serializer = PostModelSerializer(post_list, many=True)
+
+    # Report
+    client = Redis('localhost', port=6379)
+    client.set(datetime.now().strftime('%d/%m/%Y-%H:%M:%S'), f"{request.user} has retrieved a posts list")
+
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
@@ -34,6 +41,11 @@ def new_post(request):
     serializer = PostModelSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save(user=request.user)
+
+        # Report
+        client = Redis('localhost', port=6379)
+        client.set(datetime.now().strftime('%d/%m/%Y-%H:%M:%S'), f"{request.user} has created a new post")
+
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
