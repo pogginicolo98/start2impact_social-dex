f6b65e9a383e38e0630a2d410397a80b56c8e458
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-07-20 16:50:40 +0200

    Add simple posts page

--- social_dex/posts/urls.py
+++ social_dex/posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-from posts.views import HomepageView
+from posts.views import HomepageView, PostsListView
 
 urlpatterns = [
     path('', HomepageView.as_view(), name='homepage-view'),
+    path('posts/', PostsListView.as_view(), name='post-list-view'),
     path('api/', include('posts.api.urls')),
 ]
--- social_dex/posts/views.py
+++ social_dex/posts/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
-
+from django.views.generic.list import ListView
+from posts.models import Post
 
 class HomepageView(TemplateView):
     """
@@ -7,3 +8,14 @@ class HomepageView(TemplateView):
     """
 
     template_name = 'posts/homepage.html'
+
+
+class PostsListView(ListView):
+    """
+    ???
+    """
+
+    queryset = Post.objects.all().order_by('-datetime')
+    model = Post
+    template_name = 'posts/post_list.html'
+    context_object_name = 'posts'
