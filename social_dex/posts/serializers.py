30edaf85bdbcd911591cb553d9fdb8b61b246d85
Nicolò Poggi <poggi.nicolo.srvz@zohomail.eu>
2021-07-27 17:19:31 +0200

    Add bad language control system

--- social_dex/posts/serializers.py
+++ social_dex/posts/serializers.py
@@ -5,6 +5,9 @@ from rest_framework import serializers
 class PostModelSerializer(serializers.ModelSerializer):
     """
     ModelSerializer for 'Post' instance.
+
+    validations:
+    - content: Prohibits the publication of any post that contains the word 'hack'.
     """
 
     user = serializers.StringRelatedField()
@@ -12,3 +15,8 @@ class PostModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+    def validate_content(self, value):
+        if "hack" in value.lower():
+            raise serializers.ValidationError("forbidden word: 'hack'")
+        return value
